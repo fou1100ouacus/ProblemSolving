2f4753deaa696ddc1f548635db6983bc19ea0445
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2024-11-29 10:47:24 +0200

    Add files via upload
    Signed-off-by: Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>

---
+++ Array/15. 3Sum/15. 3Sum.cpp
@@ -0,0 +1,11 @@
+// 15. 3Sum.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+}
+//Input: nums = [-1, 0, 1, 2, -1, -4]
+//Output : [[-1, -1, 2], [-1, 0, 1]]
